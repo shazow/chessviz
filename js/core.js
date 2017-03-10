aefae9b6e000fabe59caa21526d8bfc257f4872d
Andrey Petrov <andrey.petrov@shazow.net>
2017-03-10 15:10:11 -0500

    cube renders

--- js/core.js
+++ js/core.js
@@ -1,26 +1,22 @@
 function init() {
-    var scene, camera, renderer;
-    var geometry, material, mesh;
+    const scene = new THREE.Scene();
 
-    scene = new THREE.Scene();
-
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
     camera.position.z = 1000;
 
-    geometry = new THREE.BoxGeometry( 200, 200, 200 );
-    material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+    const geometry = new THREE.BoxGeometry(200, 200, 200);
+    const material = new THREE.MeshBasicMaterial({
+        color: 0xff0000,
+    });
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
 
-    mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild(renderer.domElement);
 
-    document.body.appendChild( renderer.domElement );
-
+    renderer.render(scene, camera);
 }
 
-
 init();
-animate();
-
