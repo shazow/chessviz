cfe5d01a8bf79899e78fa3a46eb8c605b4754dad
Andrey Petrov <andrey.petrov@shazow.net>
2017-03-10 15:18:13 -0500

    2d board render

--- js/core.js
+++ js/core.js
@@ -1,15 +1,31 @@
 function init() {
+    const size = 10;
+
     const scene = new THREE.Scene();
 
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
-    camera.position.z = 1000;
+    camera.position.z = 100;
+    camera.position.x = size * 3.5;
 
-    const geometry = new THREE.BoxGeometry(200, 200, 200);
-    const material = new THREE.MeshBasicMaterial({
-        color: 0xff0000,
-    });
-    const mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
+    const geometry = new THREE.BoxGeometry(size, size, size);
+
+    let materials = {
+        black: new THREE.MeshBasicMaterial({
+            color: 0x333333,
+        }),
+        white: new THREE.MeshBasicMaterial({
+            color: 0xffffff,
+        })
+    }
+    const material = materials.white;
+    for (let j=0; j<8; j++) {
+        for (let i=0; i<8; i++) {
+            const box = new THREE.Mesh(geometry, material);
+            box.position.x = i * size;
+            box.position.y = j * size;
+            scene.add(box);
+        }
+    }
 
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
