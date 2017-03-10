0c88b3a7f37ed55da8530ab14936bb3591a2e078
Andrey Petrov <andrey.petrov@shazow.net>
2017-03-10 15:22:38 -0500

    alternate board colours

--- js/core.js
+++ js/core.js
@@ -1,31 +1,11 @@
 function init() {
-    const size = 10;
-
     const scene = new THREE.Scene();
 
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
     camera.position.z = 100;
-    camera.position.x = size * 3.5;
+    camera.position.x = 35;
 
-    const geometry = new THREE.BoxGeometry(size, size, size);
-
-    let materials = {
-        black: new THREE.MeshBasicMaterial({
-            color: 0x333333,
-        }),
-        white: new THREE.MeshBasicMaterial({
-            color: 0xffffff,
-        })
-    }
-    const material = materials.white;
-    for (let j=0; j<8; j++) {
-        for (let i=0; i<8; i++) {
-            const box = new THREE.Mesh(geometry, material);
-            box.position.x = i * size;
-            box.position.y = j * size;
-            scene.add(box);
-        }
-    }
+    renderBoard(scene);
 
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -33,6 +13,30 @@ function init() {
     document.body.appendChild(renderer.domElement);
 
     renderer.render(scene, camera);
+}
+
+function renderBoard(scene) {
+    const size = 10;
+
+    const colors = [
+        new THREE.MeshBasicMaterial({
+            color: 0x333333,
+        }),
+        new THREE.MeshBasicMaterial({
+            color: 0xffffff,
+        })
+    ];
+
+    const geometry = new THREE.BoxGeometry(size, size, size);
+    for (let j=0; j<8; j++) {
+        for (let i=0; i<8; i++) {
+            const box = new THREE.Mesh(geometry, colors[(i+j) % 2]);
+            box.position.x = i * size;
+            box.position.y = j * size;
+            scene.add(box);
+        }
+    }
+
 }
 
 init();
