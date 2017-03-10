8877262ef6b4334c1afa870f4b8e68a92134a7e6
Andrey Petrov <andrey.petrov@shazow.net>
2017-03-10 15:32:59 -0500

    initial board state rendering

--- js/core.js
+++ js/core.js
@@ -1,12 +1,29 @@
+const size = 10;
+
+
 function init() {
     const scene = new THREE.Scene();
 
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
-    camera.position.z = 100;
+    camera.position.z = 60;
     camera.position.x = 35;
+    camera.position.y = -50;
     camera.lookAt(new THREE.Vector3(35, 60, 0));
 
     renderBoard(scene);
+
+    const s = [
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [null, null, null, null, null, null, null, null],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+
+    renderLayer(scene, s, 1);
 
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -17,8 +34,6 @@ function init() {
 }
 
 function renderBoard(scene) {
-    const size = 10;
-
     const colors = [
         new THREE.MeshBasicMaterial({
             color: 0x333333,
@@ -38,6 +53,31 @@ function renderBoard(scene) {
         }
     }
 
+}
+
+function renderLayer(scene, state, elevation) {
+    const colors = [
+        new THREE.MeshBasicMaterial({
+            color: 0x0000dd,
+        }),
+        new THREE.MeshBasicMaterial({
+            color: 0xdd0000,
+        })
+    ];
+
+    const geometry = new THREE.BoxGeometry(size, size, size);
+    for (let j=0; j<8; j++) {
+        for (let i=0; i<8; i++) {
+            const s = state[i][j];
+            if (s == null) continue;
+
+            const box = new THREE.Mesh(geometry, colors[s]);
+            box.position.x = i * size;
+            box.position.y = j * size;
+            box.position.z = elevation * size;
+            scene.add(box);
+        }
+    }
 }
 
 init();
